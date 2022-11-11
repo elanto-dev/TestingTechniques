fa1a1d0f359bf5f0f03fb465184b34229ab7a32e
sijmen_v_b <sijmenvanbommel@gmail.com>
2022-11-11 12:03:46 +0100

    put configuration into other file to prevent merge conflicts.

---
+++ constants.py
@@ -0,0 +1,2 @@
+server_ip = '192.168.2.9'
+server_address = ''
--- testing-techniques.py
+++ testing-techniques.py
@@ -2,11 +2,13 @@ from distutils.cmd import Command
 import json
 import unittest
 import requests
+import constants
+
 
 class MatrixTestingClass(unittest.TestCase):
 
-    server_ip = '192.168.0.170'
-    server_address = ''
+    server_ip = constants.server_ip
+    server_address = constants.server_ip
 
     def setUp(self):
         self.server_address = 'https://' + self.server_ip + ':443/_matrix/client/v3'
@@ -16,16 +18,20 @@ class MatrixTestingClass(unittest.TestCase):
         request_url = self.server_address + command
         request_parameters = {
             "identifier": {
-                "type": "m.id.user", 
+                "type": "m.id.user",
                 "user": "matrixadmin"
-                }, 
-            "initial_device_display_name": "Jungle Phone", 
+            },
+            "initial_device_display_name": "Jungle Phone",
             "password": "admin", "type": "m.login.password"
         }
-        r = requests.post(url = request_url, data = json.dumps(request_parameters), verify=False)
+        r = requests.post(url=request_url, data=json.dumps(
+            request_parameters), verify=False)
         response_data = r.json()
-        self.assertIsNotNone(len(response_data["access_token"]), "access_token is missing from response")
-        self.assertGreater(len(response_data["access_token"]), 0, "access_token's length should be greater than 0")
+        self.assertIsNotNone(
+            len(response_data["access_token"]), "access_token is missing from response")
+        self.assertGreater(len(
+            response_data["access_token"]), 0, "access_token's length should be greater than 0")
+
 
 if __name__ == '__main__':
     unittest.main()
