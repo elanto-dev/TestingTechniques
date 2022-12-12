d22101797650e2856176f8c62470177cee41d29b
Sara Vahdatipour <54913233+lerunaway@users.noreply.github.com>
2022-12-12 17:02:59 +0100

    Login scenarios working

--- Assignment3/Adapter/adapter.py
+++ Assignment3/Adapter/adapter.py
@@ -1,7 +1,8 @@
 import socket
 import constants
 import apirequests as api
-
+from login import login
+seperator = '@' #this will help getting username and password from torxakis very easily
 HOST = "localhost"
 PORT = 8567
 
@@ -16,17 +17,14 @@ def handler(data):
     data = processInput(data)
     return data
 
-def processInput(message):
-    if(message.startswith("ValidLogin")):
-        return ""
-    if(message.startswith("InvalidUsernameLogin")):
-        message.replace("InvalidUsernameLogin(", "")
-        username = message[:-1]
-        return 'Response( Forbidden, \"\")\n'
-    if(message.startswith("InvalidPasswordLogin")):
-        message.replace("InvalidPasswordLogin(", "")
-        password = message[:-1]
-        return 'Response( Forbidden, \"\")\n'
+def processInput(userpass):
+        fields = userpass.split(seperator)
+        username = fields[0].strip() #Strip white spaces
+        password = fields[1].strip() 
+        response = login(username,password)
+        return response.status_code  #response is of type request (!) so I just got the status code for 
+
+
         
 condata = ConnectionData()
 condata.server_address = 'https://' + condata.server_ip + ':443/_matrix/client/v3'
@@ -38,7 +36,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     while True:
         data = conn.recv(1024)
         mess_in = data.decode()
-        print(f'received: {mess_in}')
-        mess_out = 'Response( Forbidden, \"\")\n'
+        print(f'received: {mess_in}') 
+        
+        responsecode = processInput(mess_in)
+
+        if responsecode == 200:
+            mess_out = 'Response( OK, \"\")\n'
+        elif responsecode == 403:
+            mess_out = 'Response( Forbidden, \"\")\n'
         conn.send(mess_out.encode())
         print(f'sent: {mess_out}')
